7864943d4bdaa49fdf14f30a8a4c0c331fe8918c
chunjiyun <e0908184@u.nus.edu>
2024-11-16 01:19:25 +0100

    Update report

--- components/table-component/table-component.js
+++ components/table-component/table-component.js
@@ -33,8 +33,45 @@ new gridjs.Grid({
           <img src="assets/sealant-tape.png" alt="Bagging Film" style="width:200px; height:200px;">
         </div>
       `)],
+      ["Price Per Roll (SGD)", "$1592", "5.47"],
+      ["Dimensions", gridjs.html(`
+        <div>
+          <p>Length: 304m<br/>Width: 1.22m<br/>Thickness: 0.0508mm</p>
+        </div>
+      `), "7.62m per roll"],
+      ["Elongation", "450%", "N/A"],
+      ["Material", "Nylon", "Synthetic Rubber"]
   ],
 }).render(document.getElementById("table-2"));
+
+new gridjs.Grid({
+  columns: [ 
+    { name: "Material Properties", width: '250px' },
+    { name: "SL850 Nylon Benchmark", width: '300px' },
+    { name: "Rubber", width: '200px' },
+    { name: "Silicone", width: '200px' }
+  ],
+  data: [
+      ["Temperature", "204°C", "82°C", "315°C"],
+      ["Elongation", "450%", "Up to 1000%", "Up to 1490%"],
+      ["Durability", "Single-use", "Low", "High"]
+  ],
+}).render(document.getElementById("table-3"));
+
+new gridjs.Grid({
+  columns: [ 
+    { name: "Silicone Specimen Thickness (mm)", width: '350px' },
+    { name: "Elongation At Break (mm) ", width: '300px' },
+    { name: "Elongation Rate (%)", width: '300px' },
+    { name: "Tensile Strength (kN)", width: '300px' }
+  ],
+  data: [
+      ["0.2", "167", "668", "0.0066"],
+      ["0.5", "301", "1200", "0.026433"],
+      ["1.2", "190", "760", "0.0324"],
+      ["2.0", "179", "716", "0.06055"]
+  ],
+}).render(document.getElementById("table-4"));
 
 class TableComponent extends HTMLElement {
     static get observedAttributes() {
